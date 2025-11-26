d0523ad33e7f5fbcd6d395a18918a112245932f0
nancymwende <nancymwende860@gmail.com>
2025-11-26 19:43:06 +0300

    initialise temp_conversion_tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,6 @@
+def FAHRENHEIT_TO_CELSIUS_FACTOR(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5.0 / 9.0
+def CELSIUS_TO_FAHRENHEIT_FACTOR(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return (celsius * 9.0 / 5.0) + 32
