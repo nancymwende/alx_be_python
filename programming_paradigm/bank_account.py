2e38351f2fdeb8dcbb1cbfed81db2c99efe34c1b
nancymwende <nancymwende860@gmail.com>
2025-12-04 02:37:51 +0300

    correct the deposit output to one decimsl from two decimal

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,7 +5,6 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"Deposited: ${amount:.2f}")
         else:
             print("Deposit amount must be positive.")
 
