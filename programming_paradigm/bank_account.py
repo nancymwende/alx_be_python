df7fc8d17ac8e023f976088588e2cf7c4c963466
nancymwende <nancymwende860@gmail.com>
2025-12-04 02:17:13 +0300

    initialise bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited: ${amount:.2f}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        else:
+            return False  
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.balance}")      
