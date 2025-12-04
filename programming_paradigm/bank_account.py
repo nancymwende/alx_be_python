0439e330673d78f33202569bdb6b9734d790df5f
nancymwende <nancymwende860@gmail.com>
2025-12-04 03:57:32 +0300

    Restore project

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.balance 
+
