3bbf14a589255ce1d4a564ffe7f97dbbeee9379e
nancymwende <nancymwende860@gmail.com>
2025-12-09 14:21:06 +0300

    delete pycache & modify book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,15 @@
 class Book:
-    def __init__(self, title, author, year_published):
+    
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
-        self.year_published = year_published
-
+        self.year = year
+    
     def __del__(self):
-        print(f"The book '{self.title}' by {self.author} published in {self.year_published}")
-
+        print(f"Deleting {self.title}")
+    
     def __str__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year_published})"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book(deleting instance:{self.year_published})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
